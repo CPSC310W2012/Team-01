e8faa548576b08f2e75759c19f763f51a913546a
Connor Mayer <oozie.nelson@gmail.com>
2012-02-07 12:28:04 -0800

    Added some sweet test code

---
+++ test.py
@@ -0,0 +1,18 @@
+A = [1,2,3,4,5,6,7,8]
+B = [2,4,6,8,10,12,14,16]
+inB = dict(zip(B,B))
+inBoth = []
+inA = []
+
+for key in A:
+	if inB.get(key, None):
+		inBoth.append(key)
+		del inB[key]
+	else:
+		inA.append(key)
+
+print "A: %s" % foo
+print "B: %s" % boo
+print "In A only: %s" % inA
+print "In B only: %s" % inB.keys()
+print "In both: %s" % inBoth
