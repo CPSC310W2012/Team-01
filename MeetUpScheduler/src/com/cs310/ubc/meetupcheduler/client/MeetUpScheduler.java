70dd74fc14ea3c3918710aedcb13967717373437
Caroline McQuatt <cmcquatt@gmail.com>
2012-02-12 18:16:33 -0800

    tabs added to main class. CSV upload UI in place

--- MeetUpScheduler/src/com/cs310/ubc/meetupcheduler/client/MeetUpScheduler.java
+++ MeetUpScheduler/src/com/cs310/ubc/meetupcheduler/client/MeetUpScheduler.java
@@ -1,20 +1,22 @@
 package com.cs310.ubc.meetupcheduler.client;
 
-import com.cs310.ubc.meetupcheduler.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FileUpload;
+import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteEvent;
+import com.google.gwt.user.client.ui.FormPanel.SubmitEvent;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.TabPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
@@ -24,129 +26,124 @@ public class MeetUpScheduler implements EntryPoint {
 	/**
 	 * The message displayed to the user when the server cannot be reached or
 	 * returns an error.
+	 * TODO: not implemented
 	 */
 	private static final String SERVER_ERROR = "An error occurred while "
 			+ "attempting to contact the server. Please check your network "
 			+ "connection and try again.";
+	
+	  private TabPanel tabPanel;
+	  private FormPanel form;
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT
-			.create(GreetingService.class);
 
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
+		
+	    tabPanel = new TabPanel();
+	    initTabPanel();
+	    RootPanel.get().add(tabPanel);
+	  }
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
 
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
+	private void initTabPanel() {
+		tabPanel.add(new HTML("<h1>Page 0 Content: Page 0</h1>"), " Page 0 ");
+	    tabPanel.add(new HTML("<h1>Page 1 Content: Page 1</h1>"), " Page 1 ");
+	    tabPanel.add(new HTML("<h1>Page 2 Content: Page 2</h1>"), " Page 2 ");
 
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
+	    tabPanel.addSelectionHandler(new SelectionHandler<Integer>(){
+	      public void onSelection(SelectionEvent<Integer> event) {
+	        // TODO Auto-generated method stub
+	        History.newItem("page" + event.getSelectedItem());
+	      }});
 
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
+	    History.addValueChangeHandler(new ValueChangeHandler<String>() {
+	      public void onValueChange(ValueChangeEvent<String> event) {
+	        String historyToken = event.getValue();
 
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
+	        // Parse the history token
+	        try {
+	          if (historyToken.substring(0, 4).equals("page")) {
+	            String tabIndexToken = historyToken.substring(4, 5);
+	            int tabIndex = Integer.parseInt(tabIndexToken);
+	            // Select the specified tab panel
+	            tabPanel.selectTab(tabIndex);
+	          } else {
+	            tabPanel.selectTab(0);
+	          }
 
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
+	        } catch (IndexOutOfBoundsException e) {
+	          tabPanel.selectTab(0);
+	        }
+	      }
+	    });
 
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer,
-						new AsyncCallback<String>() {
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-										.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-
-							public void onSuccess(String result) {
-								dialogBox.setText("Remote Procedure Call");
-								serverResponseLabel
-										.removeStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(result);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-						});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+	    tabPanel.selectTab(0);
+	    tabPanel.setSize("85%", "100%");
+	    //set pages here
+	    createFileUploadForm();
+	    tabPanel.add(form, "Upload CSV file");
 	}
-}
+	//TODO: CAROLINE refactor this into Admin Page class
+	private void createFileUploadForm() {
+	    // Create a FormPanel and point it at a service.
+	    form = new FormPanel();
+	    form.setAction("/csvParser");
+
+	    form.setEncoding(FormPanel.ENCODING_MULTIPART);
+	    form.setMethod(FormPanel.METHOD_POST);
+
+	    // Create a panel to hold all of the form widgets.
+	    VerticalPanel panel = new VerticalPanel();
+	    form.setWidget(panel);
+
+	    // Create a FileUpload widget.
+	    final FileUpload upload = new FileUpload();
+	    upload.setName("uploadFormElement");
+	   
+	    panel.add(upload);
+
+	    // 'submit' button.
+	    panel.add(new Button("Submit", new ClickHandler() {
+	      public void onClick(ClickEvent event) {
+	        form.submit();
+	      }
+	    }));
+
+	    // Form event handlers
+	    form.addSubmitHandler(new FormPanel.SubmitHandler() {
+	      public void onSubmit(SubmitEvent event) {
+	        if (upload.getFilename() == null || upload.getFilename().isEmpty()) {
+	        	  Window.alert("You must select a CSV file to continue");
+	        	  event.cancel();
+	        }
+	        //TODO: CAROLINE can set the filetype in the file chooser?
+	        else if (!isCSV(upload.getFilename())) {
+	        	  Window.alert("Only CSV files are supported at this time.");
+	        	  event.cancel();
+	        }
+	      }
+
+	    });
+	    form.addSubmitCompleteHandler(new FormPanel.SubmitCompleteHandler() {
+	      public void onSubmitComplete(SubmitCompleteEvent event) {
+	        // complete event
+	        Window.alert(event.getResults());
+	      }
+	    });
+		
+	}
+	//TODO: CAROLINE refactor this into Admin Page class
+	private boolean isCSV(String filename) {
+		System.out.println(filename);
+		int dot = filename.indexOf(".");
+		String type = filename.trim().substring(dot + 1).toLowerCase();
+		if (type.equals("csv")) {
+			return true;
+		}
+		return false;
+	}
+
+	
+	}
