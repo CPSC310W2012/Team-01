4438ec8300548f8a0c40c9b36dc55ed30cefed0a
unknown <Connor@Connor-PC.(none)>
2012-02-18 18:50:42 -0800

    Added skeleton for database class

--- MeetUpScheduler/src/com/cs310/ubc/meetupscheduler/client/Database.java
+++ MeetUpScheduler/src/com/cs310/ubc/meetupscheduler/client/Database.java
@@ -1,5 +1,6 @@
 package com.cs310.ubc.meetupscheduler.client;
 
+import java.util.List;
 import java.util.Map;
 
 import com.google.gwt.core.client.GWT;
@@ -11,14 +12,22 @@ public class Database {
 	private final AdvisoryServiceAsync advisoryService = GWT.create(AdvisoryService.class);
 	private final FacilityServiceAsync facilityService = GWT.create(FacilityService.class);
 	
-	public void createPark(Map<String, Object> myFields) {
-		parkService.addPark(myFields, new AsyncCallback<Void>() {
-			public void onFailure(Throwable error) {
-				
-			}
-			public void onSuccess(Void ignore) {
-				System.out.println("WOOOOOOO");
-			}
-		});
+	public void insert(String table, Object row) {
+		//TODO: Implement
 	}
+	
+	public List<Object> select(String table, String filter) {
+		//TODO: Implement
+		return null;
+	}
+	
+	public void update(String table, String column, String filter, String value) {
+		//TODO: Implement
+	}
+	
+	public void delete(String table, String filter) {
+		//TODO: Implement
+	}
+	
+	
 }
