b0f3e69e7855c91c3cd27d75006def6a97fb744b
BenCJeffery <bencjeffery@gmail.com>
2012-02-29 08:05:31 -0800

    Basic working EventView tab added.

--- MeetUpScheduler/src/com/cs310/ubc/meetupscheduler/client/EventView.java
+++ MeetUpScheduler/src/com/cs310/ubc/meetupscheduler/client/EventView.java
@@ -1,19 +1,16 @@
 package com.cs310.ubc.meetupscheduler.client;
 
-import com.cs310.ubc.meetupscheduler.server.Event;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.maps.client.Maps;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
-import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
-
 
 public class EventView extends View{
 	/**
@@ -22,9 +19,8 @@ public class EventView extends View{
 	private HorizontalPanel panel = new HorizontalPanel();
 	private Label eventName = new Label();
 	//private mapWidget eventMap = new mapWidget();
-	private Button joinButton;
-	private Button submitButton = new Button();
-	private PopupPanel joinPopup = new PopupPanel();
+	private Button joinButton = new Button();
+	private DialogBox joinBox = new DialogBox();
 	private TextBox joinName = new TextBox();
 	private ListBox attendees = new ListBox();
 	private Label eventLoc = new Label();
@@ -72,38 +68,95 @@ public class EventView extends View{
 //	}
 
 
-
 	@Override
-	public Panel createPage() {
+	public HorizontalPanel createPage() {
+		//Initialize Map, needs a key before it can be deployed
+		Maps.loadMapsApi("", "2", false, new Runnable() {
+			public void run() {
+				buildUI();
+			}
+		});
+		return panel;
+	}
+	
+	public void buildUI(){
 		//set up the eventName label
 		eventName.setText("This is the event name"); // TODO: get proper enum settings
 
 
 		//set up the joinButton
+		
+		joinButton.setText("Join event!");
 		joinButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				joinPopup.show();
+				joinBox.show();
 			}
 		});
-		joinPopup.add(joinName);
-		joinPopup.add(submitButton);
+	
+		
+		//Add the joinBox
+		setJoinBox();
 
-		submitButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				//TODO: Add the name in joinName to the Event object
-				joinPopup.hide();
-				//TODO: Refresh the attendee list after adding new attendee.
-			}
-		});
+		
+		
+		
+		attendees.addItem("Adrian");
+		attendees.addItem("Ben");
+		attendees.addItem("Caroline");
+		attendees.addItem("Connor");
+		attendees.addItem("Dave");
+		
+		
 		panel.add(eventName);
 		//panel.add(eventMap);
 		panel.add(joinButton);
 		panel.add(attendees);
-		panel.add(attendees);
 		panel.add(eventLoc);
 		panel.add(time);
 		panel.add(notes);
-		return panel;
+	}
+
+	protected void setJoinBox() {
+		
+		//Hide joinBox and set the caption
+		joinBox.hide();
+		joinBox.ensureDebugId("cwDialogBox");
+		joinBox.setText("Join this event!");
+		
+		//Create the contents of joinBox
+		VerticalPanel joinContents = new VerticalPanel();
+		joinContents.setSpacing(4);
+		joinBox.setWidget(joinContents);
+		
+		//Add to joinContents
+		joinContents.add(joinName);
+		joinName.setText("Please enter your name");
+		
+		// add the Submit Button
+		Button submitButton = new Button();
+		joinContents.add(submitButton);
+		submitButton.setText("Join");
+		submitButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+			//TODO: Set this to attendee list	
+				System.out.println(joinName.getValue());
+				joinBox.hide();
+				//TODO: Refresh the attendee list after adding new attendee.
+			}
+		});
+		//Add a cancel button
+		Button cancelButton = new Button();
+		cancelButton.setText("Cancel");
+		
+		cancelButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				joinBox.hide();
+			}
+		});
+		joinContents.add(cancelButton);
+		
+	
+		
 	}
 
 	//TODO: 
