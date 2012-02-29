b89bc6c5558034bde82a44e4b8aed8736b5d456f
davidslater250 <davidslater250@gmail.com>
2012-02-28 20:25:44 -0800

    Code cleanup, added method to test adding events to a table.

--- MeetUpScheduler/src/com/cs310/ubc/meetupscheduler/client/GlobalView.java
+++ MeetUpScheduler/src/com/cs310/ubc/meetupscheduler/client/GlobalView.java
@@ -17,40 +17,53 @@ import com.google.gwt.maps.client.geom.LatLng;
 import com.google.gwt.maps.client.overlay.Marker;
 import com.google.gwt.maps.client.control.LargeMapControl3D;
 
-
+/**
+ * view for the parks and events summary page
+ * 
+ * @author David
+ */
 
 public class GlobalView extends View {
 
+	private final int MAP_HEIGHT = 400;
+	private final int MAP_WIDTH = 500;
+	private final int EVENT_TABLE_LENGTH = 15;
+
 	private final DataObjectServiceAsync parkService = GWT.create(DataObjectService.class);
 	private final DataObjectServiceAsync eventService = GWT.create(DataObjectService.class);
-	private HorizontalPanel panel = new HorizontalPanel();
+	private HorizontalPanel rootPanel = new HorizontalPanel();
 	private VerticalPanel parkTable = new VerticalPanel();
 	private FlexTable eventTable = new FlexTable();
 	private ListBox parkBox = new ListBox();
-	private int MAPHEIGHT = 400;
-	private int MAPWIDTH = 500;
 	private ArrayList<HashMap<String, String>> allEvents;
 	private ArrayList<HashMap<String, String>> allParks;
 
+
+	/**
+	 * Loads the Maps API and returns the global view ui in a panel
+	 */
 	@Override
 	public HorizontalPanel createPage() {
-		//Initialize Map, needs a key before it can be deployed
+		//Initialize Map, needs a key before this can be deployed
 		Maps.loadMapsApi("", "2", false, new Runnable() {
 			public void run() {
 				buildUi();
 			}
 		});
-		return panel;
+		return rootPanel;
 	}
 
-
+	/**
+	 * Builds the parks and events summary page ui and places it 
+	 * in the rootPanel field
+	 */
 	private void buildUi(){
 
 		//Map
 		LatLng vancouver = LatLng.newInstance(49.258480, -123.094574);
 		final MapWidget map = new MapWidget(vancouver, 11);
 
-		map.setPixelSize(MAPWIDTH, MAPHEIGHT);
+		map.setPixelSize(MAP_WIDTH, MAP_HEIGHT);
 		map.setScrollWheelZoomEnabled(true);
 		map.addControl(new LargeMapControl3D());
 
@@ -80,11 +93,20 @@ public class GlobalView extends View {
 		eventTable.getCellFormatter().addStyleName(1, 2, "recentEventHeaders");
 		loadEvents(map);
 
-		//put panels together for return
-		panel.add(parkTable);
-		panel.add(eventTable);
+
+		//put ui elements into rootPanel field
+		rootPanel.add(parkTable);
+		rootPanel.add(eventTable);
 	}
 
+	/**
+	 * This method makes the AsyncCallback to get an ArrayList of events stored
+	 * in a HashMap. On success recent events table is loaded and event marker 
+	 * overlays are placed on the map.
+	 * 
+	 * @param map The Google Maps widget that gets the event marker overlays
+	 */
+	//TODO: popup for errors, Async for load recent events?
 	private void loadEvents(final MapWidget map){
 		eventService.get("Event", "*", new AsyncCallback<ArrayList<HashMap<String,String>>>(){
 			@Override
@@ -101,6 +123,14 @@ public class GlobalView extends View {
 		});
 	}
 
+	/**
+	 * This method makes the AsyncCallback to get an ArrayList of parks stored
+	 * in a HashMap. On success parks ListBox is loaded and park marker overlays 
+	 * are placed on the map.
+	 * 
+	 * @param map The Google Maps widget that gets the park marker overlays
+	 */
+	//TODO: popup for errors and remove markers
 	private void loadParks(final MapWidget map){
 		parkService.get("Park", "*", new AsyncCallback<ArrayList<HashMap<String,String>>>(){
 			@Override
@@ -117,11 +147,16 @@ public class GlobalView extends View {
 		});
 	}
 
-	//Add 15 most recent events to Recently Added Events table
-	//TODO:Real Events
+	/**
+	 * Adds events to the recent events table. Number of events added is
+	 * specified by the EVENT_TABLE_LENGTH field.
+	 * 
+	 * @param events The events to be added to the recent events table
+	 */
+	//TODO: time stamp?
 	private void addRecentEvents(ArrayList<HashMap<String, String>> events){
 		if(allParks != null){
-			for(int i=0; i<=14; i++){
+			for(int i=0; i<EVENT_TABLE_LENGTH; i++){
 				int row = eventTable.getRowCount();
 				String park_id = events.get(i).get("park_id");
 
@@ -137,6 +172,11 @@ public class GlobalView extends View {
 		}
 	}
 
+	/**
+	 * Adds parks to the parks ListBox.
+	 * 
+	 * @param parks The parks to be added to the parks ListBox
+	 */
 	private void addParksToListBox(ArrayList<HashMap<String, String>> parks){
 		ArrayList<String> parkNames = new ArrayList<String>();
 
@@ -151,17 +191,17 @@ public class GlobalView extends View {
 		}
 	}
 
-	private void addEventMarkers(ArrayList<HashMap<String, String>> events, MapWidget map){
-		//TODO: implement this
-		/*if(allParks != null){
-			ArrayList<String> parkId = new ArrayList<String>();
 
-			for(int i = 0; i<events.size(); i++){
-				parkId.add(events.get(i).get("park_id"));
-			}
-		}*/
+	private void addEventMarkers(ArrayList<HashMap<String, String>> events, MapWidget map){
+		//TODO: Add event markers to map widet
 	}
 
+	/**
+	 * Adds park overlay markers to the map widget
+	 * 
+	 * @param parks List of parks that will have markers placed
+	 * @param map Map widget receiving marker overlays
+	 */
 	private void addParkMarkers(ArrayList<HashMap<String, String>> parks, MapWidget map){
 		for(int i = 0; i<parks.size(); i++){
 
