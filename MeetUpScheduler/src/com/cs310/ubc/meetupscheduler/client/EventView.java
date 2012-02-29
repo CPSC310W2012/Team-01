644920f5b4af23a45b931d0ad5f5281e312b28ac
BenCJeffery <bencjeffery@gmail.com>
2012-02-28 16:11:34 -0800

    EventView changes - Comment out overloaded creator

--- MeetUpScheduler/src/com/cs310/ubc/meetupscheduler/client/EventView.java
+++ MeetUpScheduler/src/com/cs310/ubc/meetupscheduler/client/EventView.java
@@ -1,8 +1,9 @@
 package com.cs310.ubc.meetupscheduler.client;
 
 import com.cs310.ubc.meetupscheduler.server.Event;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -20,7 +21,7 @@ public class EventView extends View{
 	 */
 	private HorizontalPanel panel = new HorizontalPanel();
 	private Label eventName = new Label();
-	private mapWidget eventMap = new mapWidget();
+	//private mapWidget eventMap = new mapWidget();
 	private Button joinButton;
 	private Button submitButton = new Button();
 	private PopupPanel joinPopup = new PopupPanel();
@@ -30,25 +31,72 @@ public class EventView extends View{
 	private Label time = new Label();
 	private ListBox notes = new ListBox();
 
-	
-	
-	public Panel createPage(Event event) {
-		//TODO: Figure out how to get Event details from the JDO.
-		
+
+
+//	public Panel createPage(Event event) {
+//		//TODO: Figure out how to get Event details from the JDO.
+//
+//		//set up the eventName label
+//		//eventName.setText(event.NAME.toString()); // TODO: get proper enum settings
+//
+//
+//		//set up the joinButton
+//		joinButton.addClickHandler(new ClickHandler() {
+//			public void onClick(ClickEvent event) {
+//				joinPopup.show();
+//			}
+//		});
+//		joinPopup.add(joinName);
+//		joinPopup.add(submitButton);
+//
+//		submitButton.addClickHandler(new ClickHandler() {
+//			public void onClick(ClickEvent event) {
+//				//TODO: Add the name in joinName to the Event object
+//				joinPopup.hide();
+//				//TODO: Refresh the attendee list after adding new attendee.
+//			}
+//		});
+//
+//		//TODO: Add all attendiing members 
+//	//	attendees.addItem(event.getField()
+//		
+//		panel.add(eventName);
+//		//panel.add(eventMap);
+//		panel.add(joinButton);
+//		panel.add(attendees);
+//		panel.add(attendees);
+//		panel.add(eventLoc);
+//		panel.add(time);
+//		panel.add(notes);
+//		return panel;
+//	}
+
+
+
+	@Override
+	public Panel createPage() {
 		//set up the eventName label
-		eventName.setText(event.getField(NAME)); // TODO: Fix this
-		
+		eventName.setText("This is the event name"); // TODO: get proper enum settings
+
+
 		//set up the joinButton
-		joinButton = new Button("Join this event", new ClickListener() {
-			public void onClick(Widget sender) {
+		joinButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
 				joinPopup.show();
 			}
 		});
-		
-		
-		
+		joinPopup.add(joinName);
+		joinPopup.add(submitButton);
+
+		submitButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				//TODO: Add the name in joinName to the Event object
+				joinPopup.hide();
+				//TODO: Refresh the attendee list after adding new attendee.
+			}
+		});
 		panel.add(eventName);
-		panel.add(eventMap);
+		//panel.add(eventMap);
 		panel.add(joinButton);
 		panel.add(attendees);
 		panel.add(attendees);
@@ -58,11 +106,11 @@ public class EventView extends View{
 		return panel;
 	}
 
-		//TODO: In JAVA
-		/** joinButton.onClick - show the joinPopup, add joinName and submitButton
-		 * submitButton.onClick - add the contents of joinName to the list of attendees 
-		 * make Attendees a JDO, and persistent
-		 * 
-		 * 
-		 */
+	//TODO: 
+	/** joinButton.onClick - show the joinPopup, add joinName and submitButton
+	 * submitButton.onClick - add the contents of joinName to the list of attendees 
+	 * make Attendees a JDO, and persistent
+	 * Catch exceptions from creating jdo objects, etc.
+	 * 
+	 */
 }
