831c09224ea17d967b9078533253fc1fffb06f24
davidslater250 <davidslater250@gmail.com>
2012-03-18 12:53:28 -0700

    zoom to park button added

--- MeetUpScheduler/src/com/cs310/ubc/meetupscheduler/client/GlobalView.java
+++ MeetUpScheduler/src/com/cs310/ubc/meetupscheduler/client/GlobalView.java
@@ -5,28 +5,26 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.TabLayoutPanel;
 import com.google.gwt.user.client.ui.TabPanel;
 import com.google.gwt.maps.client.InfoWindow;
 import com.google.gwt.maps.client.InfoWindowContent;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.maps.client.MapWidget;
 import com.google.gwt.maps.client.Maps;
 import com.google.gwt.maps.client.event.MarkerClickHandler;
 import com.google.gwt.maps.client.geom.LatLng;
 import com.google.gwt.maps.client.overlay.Marker;
-import com.google.gwt.maps.client.control.Control;
-import com.google.gwt.maps.client.control.HierarchicalMapTypeControl;
 import com.google.gwt.maps.client.control.LargeMapControl3D;
-import com.google.gwt.maps.client.control.MapTypeControl;
-import com.google.gwt.maps.client.control.MenuMapTypeControl;
 
 /**
  * view for the parks and events summary page
@@ -74,16 +72,35 @@ public class GlobalView extends View{
 		//Map
 		LatLng vancouver = LatLng.newInstance(49.258480, -123.094574);
 		final MapWidget map = new MapWidget(vancouver, 11);
-
 		map.setPixelSize(MAP_WIDTH, MAP_HEIGHT);
 		map.setScrollWheelZoomEnabled(true);
-		Control typeControl = new MapTypeControl(true);
-		map.addControl(typeControl);
 		map.addControl(new LargeMapControl3D());
 
+		//Zoom to park button
+		Button parkButton = new Button("Zoom to Park", new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				if(parkBox.getItemCount()>0 && parkBox != null && allParks != null){
+					int boxIndex = parkBox.getSelectedIndex();
+					String selectedPark = parkBox.getValue(boxIndex);
+					
+					for(int i=0; i<allParks.size(); i++){
+						if(allParks.get(i).get("name").equals(selectedPark)){
+							String latLong = allParks.get(i).get("google_map_dest");
+
+							int index = latLong.indexOf(",");
+							double lat = Double.parseDouble(latLong.substring(0, index));
+							double lon = Double.parseDouble(latLong.substring(index+1));
+							
+							map.setCenter(LatLng.newInstance(lat, lon), 17);
+						}
+					}
+				}
+			}
+		});
+
 		//Parks
-		//TODO add maps functionality
 		parkTable.add(parkBox);
+		parkTable.add(parkButton);
 		parkTable.setWidth("500");
 		parkTable.add(map);
 		loadParks(map);
@@ -109,8 +126,8 @@ public class GlobalView extends View{
 		eventTabPanel.selectTab(0);
 
 
-		//put ui elements into rootPanel field
 
+		//put ui elements into rootPanel field
 		rootPanel.add(parkTable);
 		rootPanel.add(eventTabPanel);
 	}
@@ -174,23 +191,23 @@ public class GlobalView extends View{
 	 * @param events The events to be added to the recent events table
 	 *///TODO: fix the async problem here with the allParks field, add if statement (empty events)
 	private void addRecentEvents(ArrayList<HashMap<String, String>> events){
-		
+
 		int tableLength = (events.size() < EVENT_TABLE_LENGTH) ? events.size(): EVENT_TABLE_LENGTH;
-			
-			for(int i=0; i<tableLength; i++){
-				int row = recentEventsTable.getRowCount();
-				String park_id = events.get(i).get("park_id");
 
-				recentEventsTable.setText(row, 0, events.get(i).get("name"));
-				recentEventsTable.setText(row, 1, events.get(i).get("category"));
-				recentEventsTable.setText(row, 2, events.get(i).get("park_id"));
+		for(int i=0; i<tableLength; i++){
+			int row = recentEventsTable.getRowCount();
+			String park_id = events.get(i).get("park_id");
 
-				/*for(int n=0; n < allParks.size(); n++){
+			recentEventsTable.setText(row, 0, events.get(i).get("name"));
+			recentEventsTable.setText(row, 1, events.get(i).get("category"));
+			recentEventsTable.setText(row, 2, events.get(i).get("park_id"));
+
+			/*for(int n=0; n < allParks.size(); n++){
 				if(allParks.get(n).get("park_id").equals(park_id)){
 					eventTable.setText(row, 2, allParks.get(n).get("name"));
 				}
 			}*/
-			}
+		}
 	}
 
 	/**
